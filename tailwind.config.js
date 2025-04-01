4c26b8c29fbd991b810912df8957a14e5cd60a75
Gandalf-hash <phiritshepiso99@gmail.com>
2025-04-01 20:18:42 +0200

    chore(): - setup tailwind and global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,32 @@
 // filepath: tailwind.config.js
 export default {
-  darkMode: 'class', 
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  darkMode: "class",
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {},
-      fontSize: {},
+      colors: {
+        sky: "hsl(209, 23%, 22%)",
+        blue: "hsl(207, 26%, 17%)",
+        darkBlue: "hsl(200, 15%, 8%)",
+        gray: "hsl(0, 0%, 52%)",
+        lightGray: "hsl(0, 0%, 98%)",
+        white: "hsl(0, 0%, 100%)",
+      },
+      fontSize: {
+        homePageItems: "14px",
+        detailPage: "16px",
+      },
       fontFamily: {
-        sans: [],
-        serif: [],
+        sans: ["Nunito Sans", "sans-serif"],
+      },
+      fontWeight: {
+        small: "300",
+        medium: "500",
+        large: "700",
+      },
+      screen: {
+        sm: "375px",
+        lg: "1440px",
       },
     },
   },
