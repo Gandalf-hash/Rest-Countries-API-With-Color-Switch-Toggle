af6019626989933fc4e89ec93464db946e2eebf2
Gandalf-hash <phiritshepiso99@gmail.com>
2025-04-07 23:15:34 +0200

    feat(api): - added typescript types

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -1,13 +1,15 @@
-export const fetchData = async (url: string) => {
+import { Country } from "../types/Country";
+
+export const fetchData = async (url: string): Promise<Country[]> => {
   try {
     const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    const data = await response.json();
+    const data: Country[] = await response.json();
     return data;
   } catch (error) {
     console.error("Error fetching data:", error);
-    throw error; // Re-throw the error so it can be handled by the caller
+    throw error;
   }
 };
