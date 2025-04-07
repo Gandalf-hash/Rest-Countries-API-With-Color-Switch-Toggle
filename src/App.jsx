1c5e3bd4144f86a6f3871f3639cb6e547a968ab8
Gandalf-hash <phiritshepiso99@gmail.com>
2025-04-07 22:30:56 +0200

    ref(app): cleanup components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import './App.css'
+import Select from "react-select/base";
+import "./App.css";
+import { TopNavbar  } from "./components/TopNavbar";
+import { CountrySearch } from "./components/CountrySearch";
 
 function App() {
-
   return (
-  <></>
-  )
+    <div className="w-100vw h-screen bg-white dark:bg-darkBlue dark:text-white">
+      <TopNavbar />
+
+      <div className="container px-4 lg:px-12 py-[2rem] lg:items-start">
+      <CountrySearch />
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
